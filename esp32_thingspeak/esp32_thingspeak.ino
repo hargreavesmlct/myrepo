b961872ad4b08ad14e1a99aea28754f1afe8ebb7
Hargreaves <epenka71@gmail.com>
2022-11-19 15:21:21 +0800

    esp with thingspeak

---
+++ esp32_thingspeak/esp32_thingspeak.ino
@@ -0,0 +1,21 @@
+#include <WiFi.h>
+#include "ThingSpeak.h"
+
+const char* ssid = ;
+const char* pass = ;
+
+WiFiClient client;
+unsigned long myChannelNumber = 1;
+const char* myWriteAPIkey = ;
+const char* server = ;
+
+unsigned long LastTime = ;
+unsigned long timeDelay = ;
+
+void setup(){
+
+}
+
+void loop(){
+  
+}
